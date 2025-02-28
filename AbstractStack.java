d699dcfc5398d4b15ea2ec96b781898ee446aee5
Guate27 <168609891+Guate27@users.noreply.github.com>
2025-02-27 20:08:50 -0600

    Se creó la clase AbstrackStack
    Esta clase se encarga de establecer las acciones/operaciones que pueden aplicarse en una pila.

---
+++ AbstractStack.java
@@ -0,0 +1,6 @@
+abstract class AbstractStack<T> implements Stack<T> {
+    public abstract void push(T item);
+    public abstract T pop();
+    public abstract T peek();
+    public abstract boolean isEmpty();
+}
